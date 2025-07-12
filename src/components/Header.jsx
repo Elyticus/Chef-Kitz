8c99177626c73d1a52acf031168ebf20730f7e9e
igunereve <igunereve@gmail.com>
2025-07-12 17:06:00 +0300

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import chefMouseImage from "/images/mouse-chef-image.png";
+// import chefMouseImage from "/images/mouse-chef-image.png";
 
 export default function Header() {
   return (
     <header className="header">
-      <img
+      {/* <img
         className="mouse-logo"
         src={chefMouseImage}
         alt="Image of a chef mouse"
-      />
+      /> */}
       <h1 className="header-name">Chef Kitz</h1>
     </header>
   );
